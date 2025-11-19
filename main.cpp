5875135f228ecef5555d018e5fdcb7b17d999743
250132 <250132@newuu.uz>
2025-11-19 12:45:04 +0500

    week 13 lab

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+void setToHundred(int** p){
+    **p = 100;
+}
+
+    void swapRef(int a, int b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+    void swapPtr(int* a, int* b) {
+    int temp = *a;
+    *a =*b;
+    *b = temp;
+}
+
+
+    int countValid(int* arr, int size);
+
+
+
+
+int main(){
+    //problem 1
+    // int x;
+    // cin >> x;
+    // int* p = &x;
+    // setToHundred(&p);
+    // cout << x;
+    // return 0;
+
+        //problem 2
+    // int a , b ;
+    // cin >> a >> b;
+    // swapPtr(&a,&b);
+    // cout << a << " " << b << endl;
+    // return 0;
+
+    //problem 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
